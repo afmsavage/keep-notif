2b413a26587b544dbeee704f42897015ac568e27
Antonio Savage <afmsavage@gmail.com>
2020-06-27 01:52:25 -0500

    initial commit, simple discord bot working

--- index.js
+++ index.js
@@ -1,20 +1,55 @@
+const axios = require('axios').default;
+require('dotenv').config();
+const web3 = require('web3');
 
-require('dotenv').config() // process.env.xxx
+const Discord = require('discord.js'); // https://discord.js.org/
+const client = new Discord.Client();
 
-const BlocknativeSDK = require('bnc-sdk'); // https://docs.blocknative.com/notify-sdk
-const WebSocket = require('ws');
-const Web3 = require('web3');
+const permissions = '67177536';
+const tbtcContractAddr = '0x1bBE271d15Bb64dF0bc6CD28Df9Ff322F2eBD847';
+const tokenInfoUrl = `https://api.ethplorer.io/getTokenInfo/${tbtcContractAddr}?apiKey=freekey`;
+//const topTenUrl = `https://api.ethplorer.io/getTopTokenHolders/${tbtcContractAddr}?apiKey=freekey`
 
-// create options object
-const options = {
-  dappId: process.env.APIKEY,
-  networkId: 3, //ropsten, 1 for mainnet
-  //system: 'bitcoin' // optional, defaults to ethereum
-  transactionHandlers: [event => console.log(event.transaction)],
-  ws: WebSocket // only neccessary in server environments
-  name: 'tBTC' // optional, use when running multiple instances
-}
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-// initialize and connect to the api
-const blocknative = new BlocknativeSdk(options)
+client.on('message', (msg) => {
+  if (msg.content === '!help') {
+    msg.channel.send(`Available commands: !tbtc, !stats, !help`);
+  }
+  else if (msg.content === '!stats') {
+    axios
+      .get(tokenInfoUrl)
+      .then(function (response) {
+        var totalTbtc = web3.utils.fromWei(response.data.totalSupply);
+        var totalHolders = response.data.holdersCount;
 
+        console.log(totalTbtc);
+        msg.reply(`
+        Total tBTC in existence: ${totalTbtc}
+        Amount of tBTC Holders: ${totalHolders}
+        Etherscan Link to tBTC Contract: https://etherscan.io/address/0x1bbe271d15bb64df0bc6cd28df9ff322f2ebd847`
+        );
+      })
+      .catch(function (error) {
+        msg.reply("Well that's funky, seem to have run into an error with that request.  Please try again")
+        console.log(error);
+      });
+  }
+  else if (msg.content === '!tbtc') {
+    axios
+      .get(tokenInfoUrl)
+      .then(function (response) {
+        var totalTbtc = web3.utils.fromWei(response.data.totalSupply);
+        console.log(totalTbtc);
+        msg.reply(`Total tBTC in existence: ${totalTbtc}`).supressEmbeds(true);
+      })
+      .catch(function (error) {
+        msg.reply("Too much tBTC for me to handle I guess, please try again")
+        console.log(error);
+      });
+  }
+});
+
+client.login(process.env.BOTTOKEN);
